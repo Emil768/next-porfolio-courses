18d66828f4f953ba8b1db6763b676e912527779d
EmilMurahas228 <murakhas@list.ru>
2023-01-22 15:33:09 +0300

    feat: Add env in next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
     deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 
+  env: {
+    SERVER: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
